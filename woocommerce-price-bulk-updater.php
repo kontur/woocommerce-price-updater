b679bd591b26f1e194e5629695e019cffa563f7a
kontur <kontur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-08-21 08:19:01 +0000

    added woocommerce warning text
    git-svn-id: https://plugins.svn.wordpress.org/woo-price-updater/trunk@1927832 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-price-bulk-updater.php
+++ woocommerce-price-bulk-updater.php
@@ -7,7 +7,7 @@
  * WC Tested Up To: 3.2.6
  * Author:      	Underscore
  * Author URI:  	https://underscoretype.com
- * Copyright:   	Copyright 2017-2018 Johannes Neumeier
+ * Copyright:   	Copyright 2018 Johannes Neumeier
  */
 defined('ABSPATH') or die('Access denied.');
 
@@ -15,8 +15,8 @@ define('PRICE_BULK_UPDATER_NAMESPACE', 'woocommerce-price-bulk-updater');
 define('PRICE_BULK_UPDATER_ADMIN_URL_NAME', 'price-bulk-updater');
 define('PRICE_BULK_UPDATER_ADMIN_URL', 'edit.php?post_type=product&page=' . PRICE_BULK_UPDATER_ADMIN_URL_NAME);
 
-require_once plugin_dir_path(__FILE__) . 'classes/Price_Bulk_Updater.php';
+require_once plugin_dir_path(__FILE__) . 'classes/Price_Bulk_Updater_Plugin.php';
 require_once plugin_dir_path(__FILE__) . 'classes/Price_Bulk_Updater_Product_Search.php';
-$updater = new Price_Bulk_Updater();
+$updater = new Price_Bulk_Updater_Plugin();
 
-register_activation_hook(__FILE__, array('Price_Bulk_Updater', 'hook_activate'));
+register_activation_hook(__FILE__, array('Price_Bulk_Updater_Plugin', 'hook_activate'));
