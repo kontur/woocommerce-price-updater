01ea86b31834ed20a654783a398f1384d3bca64a
kontur <kontur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-08-21 08:20:27 +0000

    updated assigned namespace
    git-svn-id: https://plugins.svn.wordpress.org/woo-price-updater/trunk@1927837 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-price-updater.php
+++ woocommerce-price-updater.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: 	WooCommerce Price Updater
- * Plugin URI:		https://wordpress.org/plugins/woocommerce-price-updater
+ * Plugin Name: 	Woo Price Updater
+ * Plugin URI:		https://wordpress.org/plugins/woo-price-updater
  * Description:		A simple Wordpress WooCommerce plugin to set or replace WooCommerce prices in bulk
  * Version:     	0.1.0
  * WC Tested Up To: 3.2.6
@@ -11,8 +11,8 @@
  */
 defined('ABSPATH') or die('Access denied.');
 
-define('WOOCOMMERCE_PRICE_UPDATER_NAMESPACE', 'woocommerce-price-updater');
-define('WOOCOMMERCE_PRICE_UPDATER_ADMIN_URL_NAME', 'woocommerce-price-updater');
+define('WOOCOMMERCE_PRICE_UPDATER_NAMESPACE', 'woo-price-updater');
+define('WOOCOMMERCE_PRICE_UPDATER_ADMIN_URL_NAME', 'woo-price-updater');
 define('WOOCOMMERCE_PRICE_UPDATER_ADMIN_URL', 'edit.php?post_type=product&page=' . WOOCOMMERCE_PRICE_UPDATER_ADMIN_URL_NAME);
 
 require_once plugin_dir_path(__FILE__) . 'classes/Woocommerce_Price_Updater_Plugin.php';
